4954e041af8ae89d86ba3468f29a0ecae576b74a
kobato <lfkobato@gmail.com>
2025-06-09 14:04:29 +0900

    バージョンビルド番号を自動インクリメントするIncrementMinorVersion.csを追加（テスト段階） ベースHTMLの微修正（2行追加）

---
+++ src/MJS_WordPlugin_src/WordAddIn1/IncrementMinorVersion.ps1
@@ -0,0 +1,12 @@
+﻿# IncrementMinorVersion.ps1
+$path = "WordAddIn1\Properties\AssemblyInfo.cs"
+$versionPattern = '\[assembly: Assembly(File)?Version\("3\.0\.(\d+)\.0"\)\]'
+(Get-Content $path) | ForEach-Object {
+    if ($_ -match $versionPattern) {
+        $current = [int]$matches[2]
+        $next = $current + 1
+        $_ -replace "3\.0\.\d+\.0", "3.0.$next.0"
+    } else {
+        $_
+    }
+} | Set-Content $path
