a6ac4b217a57232fdf6caa2d07ab3e1b43db3f46
kobato <lfkobato@gmail.com>
2025-06-10 10:10:04 +0900

    リリースビルドの環境設定変更

--- src/MJS_WordPlugin_src/WordAddIn1/IncrementMinorVersion.ps1
+++ src/MJS_WordPlugin_src/WordAddIn1/IncrementMinorVersion.ps1
@@ -1,11 +1,10 @@
-﻿# IncrementMinorVersion.ps1
+﻿# WordAddIn1\Properties\AssemblyInfo.cs のビルド番号（3桁目）を+1する
 $path = "WordAddIn1\Properties\AssemblyInfo.cs"
-$versionPattern = '\[assembly: Assembly(File)?Version\("3\.0\.(\d+)\.0"\)\]'
+$pattern = '(\[assembly: Assembly(File)?Version\("3\.1\.)(\d+)(\.0"\)\])'
 (Get-Content $path) | ForEach-Object {
-    if ($_ -match $versionPattern) {
-        $current = [int]$matches[2]
-        $next = $current + 1
-        $_ -replace "3\.0\.\d+\.0", "3.0.$next.0"
+    if ($_ -match $pattern) {
+        $newBuild = [int]$matches[3] + 1
+        $_ -replace "3\.1\.\d+\.0", "3.1.$newBuild.0"
     } else {
         $_
     }
