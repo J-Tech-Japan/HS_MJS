d41dbc4ff65b03fc01f8824e398a42b726f8658c
AtelierKobato <lfkobato@gmail.com>
2026-01-19 11:21:12 +0900

    Delete src/helpcenter/LucaTech_HC(20251017a)/contents directory

--- src/helpcenter/LucaTech_HC(20251017a)/contents/CMN_JNT/whxdata/toc1.new.js
+++
@@ -1,4 +0,0 @@
-﻿(function() {
-var toc =  [{"type":"item","name":"はじめに","url":"CMN11001.html"},{"type":"book","name":"導入処理","key":"toc2"}];
-window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
-})();
--- src/helpcenter/LucaTech_HC(20251017a)/contents/CMN_JNT/whxdata/toc3.new.js
+++
@@ -1,4 +0,0 @@
-﻿(function() {
-var toc =  [{"type":"item","name":"カレンダーグループを作成する","url":"CMN11007.html"},{"type":"item","name":"カレンダーを作成する","url":"CMN11010.html"}];
-window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
-})();
